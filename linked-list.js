c1d6cb8692452917fd61cb9f4bf6b8cf26d56906
aya-alhusamia <https://github.com/aya-alhusamia/Git-Task>
2021-05-26 16:33:20 +0300

    solved  years

--- linked-list.js
+++ linked-list.js
@@ -6,70 +6,44 @@ class Node{
         this.nextNode=nextNode
     }
 }
+
 class LinkedList{
-    constructor(year, highlight, nextNode){
-        this.headNoed= new Node(year, highlight, nextNode)
+    constructor(year, highlight){
+        this.headNoed= new Node(year, highlight)
     }
 
-addNode=(year,highlight)=>{
-    const newNode= new Node(year,highlight);
-     newNode.nextNode = this.headNoed
-     this.headNoed=newNode
+insertBegening=(year,highlight)=>{
+    const newNode= new Node(year,highlight,this.headNoed);
+     this.headNoed= newNode
 };
-addingAfter = (year,highlight,afterYear)=>{
-    let currentNode= this.headNoed
-    let nwNode= new Node(year, highlight)
-    if(currentNode.year ===afterYear){
-        nwNode.nextNode=currentNode.nextNode
-        currentNode.nextNode=nwNode
+getLinkList=()=>{
+    let currnt = this.headNoed
+    while(currnt){
+        console.log(`year: ${currnt.year}, hightlight:${currnt.highlight}`)
+          currnt= currnt.nextNode
     }
-    else{
-        while(currentNode){
-            if(currentNode.year===afterYear){
-                nwNode.nextNode=currentNode.nextNode
-                currentNode.nextNode=nwNode
-            }
-            currentNode=currentNode.nextNode
+}
+insetHightlight= (year)=>{
+    let currnt = this.headNoed
+    while(currnt.year <year){
+        let currentAge = currnt.year+1
+        if(currnt.nextNode && currnt.nextNode.year === currentAge){
+            currnt= currnt.nextNode
+        }
+        else{
+            let highlight = prompt(`Enter hightlight for year :${currentAge} `)
+            let newNode= new Node(currentAge,highlight,currnt.nextNode)
+            currnt.nextNode= newNode
+            currnt=newNode
         }
     }
 }
-outOFyear=(year)=>{
-    let currentNode= this.headNoed
-    let nw= currentNode.year
-    if(nw===year){
-        return true
-    }
-    else {
-        while(currentNode){
-            if(nw === year){
-                return true
-            }
-            currentNode=currentNode.nextNode
-        }
-         
-    }
-    return false
-} 
-    
-get dataAsString() {
-    let dataString = "";
-    let currentNode = this.headNode;
-    while (currentNode && currentNode.year < year) {
-      dataString = dataString + `year: ${currentNode.year},highlight:${currentNode.highlight}  \n`;
-      currentNode = currentNode.nextNode;
-    }
-    return dataString;
-  }
+
 }
-const myList = new LinkedList(1, "I was born");
-myList.addingAfter(3,"I started walking",1)
-myList.addingAfter(7,"I turned seven",3)
-console.log(myList.addNode)
-const ageIs=prompt("What is your age?")
-for(let x =1; x<ageIs;x++){
-    if(!myList.outOFyear(x)){
-        let highlight = prompt("enter the highlight"+x+": ")
-        myList.addingAfter(x , highlight,x+1)
-    }
-}
-console.log(myList.dataAsString(ageIs))
+const ageList = new LinkedList(7 , "I turned seven")
+ageList.insertBegening(3,"i starting walking")
+ageList.insertBegening(1,"i was born")
+let age = prompt("age: ")
+ageList.insetHightlight(age)
+ageList.getLinkList()
+ 
